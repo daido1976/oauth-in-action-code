f57292993c430cbc87f48551ccbac39df0643d65
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-08-03 12:18:12 +0900

    Implement userInfoEndpoint

--- exercises/ch-13-ex-1/protectedResource.js
+++ exercises/ch-13-ex-1/protectedResource.js
@@ -85,6 +85,72 @@ var userInfoEndpoint = function (req, res) {
   /*
    * Implement the UserInfo Endpoint
    */
+  if (!__.contains(req.access_token.scope, "openid")) {
+    res.status(403).end();
+    return;
+  }
+
+  let user = req.access_token.user;
+  if (!user) {
+    res.status(404).end();
+    return;
+  }
+
+  let out = {};
+  __.each(req.access_token.scope, (scope) => {
+    switch (scope) {
+      case "openid":
+        __.each(["sub"], (claim) => {
+          if (user[claim]) {
+            out[claim] = user[claim];
+          }
+        });
+      case "profile":
+        __.each(
+          [
+            "name",
+            "family_name",
+            "given_name",
+            "middle_name",
+            "nickname",
+            "preferred_username",
+            "profile",
+            "picture",
+            "website",
+            "gender",
+            "birthdate",
+            "zoneinfo",
+            "locale",
+            "updated_at",
+          ],
+          (claim) => {
+            if (user[claim]) {
+              out[claim] = user[claim];
+            }
+          }
+        );
+      case "email":
+        __.each(["email", "email_verified"], (claim) => {
+          if (user[claim]) {
+            out[claim] = user[claim];
+          }
+        });
+      case "address":
+        __.each(["address"], (claim) => {
+          if (user[claim]) {
+            out[claim] = user[claim];
+          }
+        });
+      case "phone":
+        __.each(["phone_number", "phone_number_verified"], (claim) => {
+          if (user[claim]) {
+            out[claim] = user[claim];
+          }
+        });
+    }
+  });
+  res.status(200).json(out);
+  return;
 };
 
 app.get("/userinfo", getAccessToken, requireAccessToken, userInfoEndpoint);
